91825060a433380b122d7e22120325d89d11f609
Christian Jimenez <cajt@outlook.com>
2024-09-25 11:36:58 -0800

    Excercise 8

--- ex08.js
+++ ex08.js
@@ -1,3 +1,9 @@
 // Join Exercise
-// Instructions: Create an array of at least five different words as strings. Write code to join all the words into a single string, with each word separated by a space.
+// Instructions: Create an array of at least five different words as 
+//strings. Write code to join all the words into a single string, with 
+//each word separated by a space.
+
+const stringArr = ['Hi,','How','are','you','?']
+
+console.log(stringArr.join(" "))
 
