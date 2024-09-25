d4e80b7da3f439d96946eb4ed31ecd0fb917aad0
Christian Jimenez <cajt@outlook.com>
2024-09-25 11:34:16 -0800

    Excersice 7

--- ex07.js
+++ ex07.js
@@ -1,3 +1,8 @@
 // IndexOf Exercise
-// Instructions: Create an array of at least five different fruits as strings. Write code to find the index of the fruit "banana".
+// Instructions: Create an array of at least five different fruits as 
+//strings. Write code to find the index of the fruit "banana".
 
+
+const fruits = ["apple","orange","mango", "banana","lemon","pear"]
+
+console.log(fruits.indexOf("banana"))
