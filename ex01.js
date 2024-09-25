9bb2c2801590e911af610f99938f0433c108be40
Christian Jimenez <cajt@outlook.com>
2024-09-25 11:21:06 -0800

    Excersice 1

--- ex01.js
+++ ex01.js
@@ -1,2 +1,13 @@
 // Sort Exercise
-// Instructions: Create an array of at least five numbers in random order. Write the code to sort this array in ascending order.
+// Instructions: Create an array of at least five numbers in random order. 
+//Write the code to sort this array in ascending order.
+
+const numArr = [3, 7, 34, 90, 1]
+
+function bubbleSort(a,b){
+    if(a > b) return 1
+    if(a < b) return -1
+    return 0;
+}
+
+console.log(numArr.sort(bubbleSort))
