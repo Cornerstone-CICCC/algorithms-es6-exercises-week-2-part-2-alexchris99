b7ede01e3bd4e79559dcd2c5494a077627fea675
Christian Jimenez <cajt@outlook.com>
2024-09-25 11:32:50 -0800

    Excersice 6

--- ex06.js
+++ ex06.js
@@ -1,3 +1,15 @@
 // Every Exercise
-// Instructions: Create an array of at least five numbers. Write code to check if every number in the array is positive.
+// Instructions: Create an array of at least five numbers. 
+//Write code to check if every number in the array is positive.
 
+const numArr = [23, 40, 80, 25, 60, 2]
+
+const Positive = numArr.every((item) =>{
+    if(item  < 0){
+        return false
+    }else{
+        return true
+    }
+})
+
+console.log(Positive)
