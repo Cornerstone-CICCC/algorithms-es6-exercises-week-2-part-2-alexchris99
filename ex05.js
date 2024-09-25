dc150524f481b1a45d09989262eb5464fc337411
Christian Jimenez <cajt@outlook.com>
2024-09-25 11:29:26 -0800

    Excersice 5

--- ex05.js
+++ ex05.js
@@ -1,3 +1,11 @@
 // Some Exercise
-// Instructions: Create an array of at least five numbers. Write code to check if some of the numbers are even.
+// Instructions: Create an array of at least five numbers. 
+//Write code to check if some of the numbers are even.
 
+const numArr = [23, 40, 80, 25, 60, 2]
+
+const even = (element) =>{
+    return element % 2 === 0;
+}
+
+console.log(numArr.some(even))
